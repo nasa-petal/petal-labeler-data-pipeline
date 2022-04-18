ced5cc9fd5808b30e8da4ec35a5dfa52322c917b
dsmith111 <davidcsmith12@gmail.com>
2022-04-18 14:33:36 -0400

    Remove garbled text

--- LabeledData/convert_with_api.py
+++ LabeledData/convert_with_api.py
@@ -60,6 +60,8 @@ def build_abstract(inverted_ind:dict):
     for word in inverted_ind.keys():
         for index in inverted_ind[word]:
             abstract_list[index] = word
+    full_abstract = " ".join(abstract_list)
+    full_abstract = re.sub(" -(.*)- ", " ", full_abstract)
 
     return " ".join(abstract_list)
 
@@ -232,6 +234,9 @@ def convert_to_json(dataframe: pd.DataFrame, api_res: list, api_dois: list):
             # temp_dict["ask_level3"] = row.get("ask_label_level_3", [])
             temp_dict["isBiomimicry"] = row.get("isBiomimicry", "undetermined")
             temp_dict["url"] = row["url"]
+            temp_dict["species"] = row["species"]
+            temp_dict["absolute_relevancy"] = row["absolute_relevancy"]
+            temp_dict["relative_relevancy"] = row["relative_relevancy"]
             if (row.get("mag_terms", False)):
                 if (type(row["mag_terms"]) == str):
                     temp_dict["mag_terms"] = eval(row["mag_terms"])
